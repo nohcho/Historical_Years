4b387d78d7600efeff372502df677bab15415509
Muhammad <tasuhanovw@mail.ru>
2025-05-14 21:53:18 +0300

    add localIdentName

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
+const isProd = process.env.NODE_ENV === 'production';
+
 module.exports = {
     stats: 'errors-only',
 
@@ -10,19 +12,19 @@ module.exports = {
         filename: 'bundle.js',
         clean: true,
     },
+
     resolve: {
         extensions: ['.tsx', '.ts', '.js'],
         alias: {
-            '@': path.resolve(__dirname, 'src'),
-
+            '@':  path.resolve(__dirname, 'src'),
             src: path.resolve(__dirname, 'src'),
-        },    },
+        },
+    },
+
     module: {
         rules: [
-            {
-                test: /\.svg$/,
-                use: ['@svgr/webpack'],
-            },
+            { test: /\.svg$/, use: ['@svgr/webpack'] },
+
             {
                 test: /\.(ts|tsx|js|jsx)$/,
                 exclude: /node_modules/,
@@ -36,56 +38,57 @@ module.exports = {
                     {
                         loader: 'css-loader',
                         options: {
-                            modules: true,
+                            modules: {
+                                localIdentName: isProd
+                                    ? '[hash:base64:6]'
+                                    : '[path][name]__[local]--[hash:base64:5]',
+                            },
+                            sourceMap: !isProd,
                         },
                     },
                     {
                         loader: 'sass-loader',
                         options: {
-                            implementation: require('sass'), // <-- ВАЖНО
-                            sassOptions: {
-                                // Скрывает предупреждения о "legacy JS API" в зависимостях
-                                quietDeps: true,
-                            },
+                            implementation: require('sass'),
+                            sassOptions: { quietDeps: true },
+                            sourceMap: !isProd,
                         },
-
                     },
                 ],
             },
+
             {
                 test: /\.s[ac]ss$/,
                 exclude: /\.module\.s[ac]ss$/,
                 use: [
                     'style-loader',
-                    'css-loader',
+                    {
+                        loader: 'css-loader',
+                        options: { sourceMap: !isProd },
+                    },
                     {
                         loader: 'sass-loader',
                         options: {
                             implementation: require('sass'),
-                            sassOptions: {
-                                includePaths: [path.resolve(__dirname, 'src')],
-                            },
+                            sassOptions: { includePaths: [path.resolve(__dirname, 'src')] },
+                            sourceMap: !isProd,
                         },
                     },
                 ],
             },
         ],
     },
+
     plugins: [
-        new HtmlWebpackPlugin({
-            template: './public/index.html',
-        }),
+        new HtmlWebpackPlugin({ template: './public/index.html' }),
     ],
+
     devServer: {
         static: './dist',
         hot: true,
         port: 4000,
-        client: {
-            overlay: {
-                errors: true,
-                warnings: false,
-            },
-        },
+        client: { overlay: { errors: true, warnings: false } },
     },
-    mode: 'development',
+
+    mode: isProd ? 'production' : 'development',
 };
